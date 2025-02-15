a94c07695afcd2408b3f009bf45a81c6115031f5
I-Love-Jesus-Christ <starmergedovercrosses@gmail.com>
2025-02-15 13:24:35 -0500

    Do task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,27 +18,29 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const book = books[req.params.isbn];
-  if (book) {
+  const ISBN = req.params.isbn;
+  const book = books[ISBN];
+  if (book != undefined) {
     return res.status(200).json(book);
   } else {
-    return res.status(404).json({message: "Not Found"});
+    return res.status(200).json({message: `No book with ISBN ${ISBN} found.`});
   }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   let target_books = [];
+  const author = req.params.author;
   for (key in books) {
     let book = books[key];
-    if (req.params.author == book["author"]) {
+    if (author == book["author"]) {
       target_books.push(book);
     }
   }
   if (target_books.length > 0) {
     return res.status(200).json(target_books);
   } else {
-    return res.status(200).json({message: `No book of author ${req.params.author} found.`})
+    return res.status(200).json({message: `No book of author ${author} found.`})
   }
   
 });
@@ -46,23 +48,36 @@ public_users.get('/author/:author',function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   let target_books = [];
+  const title = req.params.title;
   for (key in books) {
     let book = books[key];
-    if (req.params.title == book["title"]) {
+    if (title == book["title"]) {
       target_books.push(book);
     }
   }
   if (target_books.length > 0) {
     return res.status(200).json(target_books);
   } else {
-    return res.status(200).json({message: `No book titled ${req.params.title} found.`})
+    return res.status(200).json({message: `No book titled ${title} found.`})
   }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const ISBN = req.params.isbn;
+  let target_book = books[ISBN];
+  let reviews;
+  if (target_book != undefined) {
+    reviews = target_book.reviews;
+    if (Object.keys(reviews).length > 0) {
+      return res.status(200).json(reviews);
+    } else {
+      return res.status(200).json({message: `No reviews for the book with the ISBN ${ISBN} have been written yet.`})
+    }
+  } else {
+    return res.status(200).json({message: `No book with the ISBN ${ISBN} found.`})
+  }
+  
 });
 
 module.exports.general = public_users;
