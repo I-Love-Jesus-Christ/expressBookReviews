56e9a9feb81cd6e3f98fbc6a83f3a62f33ba89c2
I-Love-Jesus-Christ <starmergedovercrosses@gmail.com>
2025-02-15 01:25:10 -0500

    Do task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,14 +24,23 @@ public_users.get('/isbn/:isbn',function (req, res) {
   } else {
     return res.status(404).json({message: "Not Found"});
   }
-
-
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let target_books = [];
+  for (key in books) {
+    let book = books[key];
+    if (req.params.author == book["author"]) {
+      target_books.push(book);
+    }
+  }
+  if (target_books.length > 0) {
+    return res.status(200).json(target_books);
+  } else {
+    return res.status(200).json({message: `No book of author ${req.params.author} found.`})
+  }
+  
 });
 
 // Get all books based on title
