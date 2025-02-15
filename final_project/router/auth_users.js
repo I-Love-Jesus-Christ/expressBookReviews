028e3cf8563bce573035c65a99cff78ffbf4659b
I-Love-Jesus-Christ <starmergedovercrosses@gmail.com>
2025-02-15 16:09:42 -0500

    Do task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,64 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+// Redundant function. 
+const isValid = (username) => {  
+  let target_username_found = false;
+  let current_position = 0;
+  while (current_position < users.length && target_username_found == false) {
+    let current_user = users[current_position];
+    if (current_user.username == username) {
+      target_username_found = true;
+    }
+    current_position += 1;
+  }
+
+  return target_username_found;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+      return (user.username === username && user.password === password);
+  });
+  // Return true if any valid user is found, otherwise false
+  if (validusers.length > 0) {
+      return true;
+  } else {
+      return false;
+  }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+
+    // Check if the username is valid.  Redundant and waste of time operation. Duplication of part of the authenticatedUser operation.
+    if (!isValid(username)) {
+      return res.status(404).json({message: "Invalid username"})
+    }
+
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
