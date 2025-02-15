0b73ad1bedc044383dbef6d646d7965099bd71fe
I-Love-Jesus-Christ <starmergedovercrosses@gmail.com>
2025-02-15 00:50:26 -0500

    Do task 1 and task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,20 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const book = books[req.params.isbn];
+  if (book) {
+    return res.status(200).json(book);
+  } else {
+    return res.status(404).json({message: "Not Found"});
+  }
+
+
  });
   
 // Get book details based on author
